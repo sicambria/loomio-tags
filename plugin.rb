a303cd35e629774dfde9736635f107a3548798b0
gaspar <inczegaspar@gmail.com>
2016-05-22 17:28:43 +0200

    Enable tag plugin

--- plugin.rb
+++ plugin.rb
@@ -3,6 +3,7 @@ module Plugins
     class Plugin < Plugins::Base
       setup! :loomio_tags do |plugin|
         # our code will go here!
+        plugin.enabled = ‘ENABLE_THREAD_TAGGING’
         puts "loomio_tags: Hello, world!"
       end
     end
